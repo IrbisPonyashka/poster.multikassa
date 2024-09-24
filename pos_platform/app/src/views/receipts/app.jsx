23437ae383836dc35db3707be0b7765dc812b2a3
Ismail <ismail.gaibov@micros.uz>
2024-09-24 02:36:19 +0000

    dev_manage_platform

--- pos_platform/app/src/views/receipts/app.jsx
+++ pos_platform/app/src/views/receipts/app.jsx
@@ -15,15 +15,34 @@ export default function Receipts() {
     useEffect( () => {
         getReceiptsRequest();
     }, []);
+    
+    const [totalRows, setTotalRows] = useState(0);
+    
+    const [rowData, setRowData] = useState([]);
+    
+    const [colDefs, setColDefs] = useState(getColumnsData);
 
-    const getReceiptsRequest = async () => {
+    const onPaginationChanged = (params) => {
+        console.log("onPaginationChanged", params);
+        if (params.api) {
+            const currentPage = params.api.paginationGetCurrentPage() + 1;  // Текущая страница (нумерация с 0)
+            const pageSize = params.api.paginationGetPageSize();  // Количество записей на странице
+            
+            // Запросить данные для текущей страницы
+            getReceiptsRequest(currentPage, pageSize);
+        }
+    };
+
+    const getReceiptsRequest = async ( page = 1, limit = 15 ) => {
         return new Promise((resolve, reject) => {
+            const start = (page - 1) * limit;
+
             const requestOptions = {
                 method: "GET",
                 redirect: "follow"
               };
               
-              fetch("http://localhost:8080/api/v1/receipts?page=1&start=0&limit=15", requestOptions)
+              fetch(`http://localhost:8080/api/v1/receipts?page=${page}&start=${start}&limit=${limit}`, requestOptions)
                 .then((response) => response.text())
                 .then((result) => {
                     result = JSON.parse(result);
@@ -35,11 +54,8 @@ export default function Receipts() {
         })
     }
     
-    const [rowData, setRowData] = useState([]);
-    
-    const [colDefs, setColDefs] = useState(getColumnsData);
 
-    // console.log("receipts", receipts);
+    console.log("receipts", rowData);
     return (
         <Content id="receipts" >
             <div className="ag-theme-quartz" style={{ flex: '1 1 auto', minHeight: 0 }}>
@@ -47,6 +63,10 @@ export default function Receipts() {
                     rowData={rowData}
                     columnDefs={colDefs}
                     domLayout='autoHeight'
+                    pagination={false}  // Включаем пагинацию
+                    // paginationPageSize={10}  // Количество записей на одной странице
+                    // paginationPageSizeSelector={[ 10, 20, 50, 100 ]}
+                    // onPaginationChanged={onPaginationChanged}  // Обработка изменения страницы
                 />
             </div>
         </Content>
--- pos_platform/app/src/views/receipts/colData.js
+++ pos_platform/app/src/views/receipts/colData.js
@@ -1,29 +1,38 @@
 export default function getColumnsData() {
 
+    const operationTypeMapping = {
+        1: "Открытие смены",
+        2: "Закрытие смены",
+        3: "Продажа",
+        4: "Возврат",
+        7: "X отчет",
+        8: "Авансовый чек",
+        9: "Кредитный чек",
+    };
+
     return [
         { 
             field: "receipt_id",
             headerName: "ID чека" ,
+            hide: true, 
         },
         { 
             field: "receipt_gnk_receiptseq",
             headerName: "Номер чека" ,
+            valueFormatter: params => String(params.value),
         },
         { 
             field: "module_operation_type",
             headerName: "Операция" ,
+            valueFormatter: params => operationTypeMapping[params.value] || params.value,
         },
         {
             field: "receipt_cashier_name",
             headerName: "Кассир",
         },
         {
-            field: "receipt_gnk_datetime",
+            field: "receipt_gnk_time",
             headerName: "Время",
-        },
-        {
-            field: "payment_type",
-            headerName: "Тип оплаты",
         },
         {
             field: "module_operation_sum_card",
